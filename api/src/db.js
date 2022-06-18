28b0ef0a4539506e89686ac723a18d1aadb6c076
Tomas <tomas8olivera@gmail.com>
2022-06-18 20:18:09 -0300

    db created

--- api/src/db.js
+++ api/src/db.js
@@ -3,8 +3,8 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+  DB_USER, DB_PASSWORD, DB_HOST,                 //destructuring de mis variables de entorno
+} = process.env; 
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
   logging: false, // set to console.log to see the raw SQL queries
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Recipe.belongsToMany(Diet, {through: 'Recipe_Diet'})
+Diet.belongsToMany(Recipe, {through: 'Recipe_Diet'})
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,28 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    resumen: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    health_score: {
+      type: DataTypes.FLOAT,
+      validate: {
+        min: 0,
+        max: 100,
+      }
+    },
+    step_by_step: {
+      type: DataTypes.STRING,
+    }
   });
 };
